384c96fff1f5df59ce1bbb000b2a7d5a78359dda
melodysu83 <melodysu83@gmail.com>
2021-02-01 13:05:56 -0500

    add console prints and options to choose between examples

--- main.py
+++ main.py
@@ -16,68 +16,86 @@ import os
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
+
 mychoice = input("Choose 1 for Membrane example, 2 for Endoscopic images.")
+myaction = input("Choose 1 for Training, 2 for Testing.")
+
 
 if mychoice == 1:
-    data_gen_args = dict(rotation_range=0.2,
-                    width_shift_range=0.05,
-                    height_shift_range=0.05,
-                    shear_range=0.05,
-                    zoom_range=0.05,
-                    horizontal_flip=True,
-                    fill_mode='nearest')
-    myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
+    if myaction == 1:
+        print("Need to pull stuff from trainUnet.ipynb")
+    elif myaction == 2:
+        data_gen_args = dict(rotation_range=0.2,
+                        width_shift_range=0.05,
+                        height_shift_range=0.05,
+                        shear_range=0.05,
+                        zoom_range=0.05,
+                        horizontal_flip=True,
+                        fill_mode='nearest')
+        myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
 
-    model = unet()
-    model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-    model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
+        model = unet()
+        model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
+        model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
 
-    testGene = testGenerator("data/membrane/test")
-    results = model.predict_generator(testGene,30,verbose=1)
-    saveResult("data/membrane/test",results)
+        testGene = testGenerator("data/membrane/test")
+        results = model.predict_generator(testGene,30,verbose=1)
+        saveResult("data/membrane/test",results)
+    else:
+        print("Invalid action. Goodbye!")
 
 elif mychoice == 2:
-    
-    #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-    # Takes as input path to image file and returns 
-    # resized 3 channel RGB image of as numpy array of size (256, 256, 3)
-    def getPic(img_path):
-        return np.array(Image.open(img_path).convert('RGB').resize((256,256),Image.ANTIALIAS))
+    if myaction == 1:
+        print("Need to pull stuff from trainUnet.ipynb")
 
-    # Return the images and corresponding labels as numpy arrays
-    def get_ds(data_path, label_path):
-        img_paths = list()
-        lbl_paths = list()
-        
-        # Recursively find all the image files from the path data_path
-        for img_path in glob.glob(data_path+"/**/*"):
-            img_paths.append(img_path)
-        
-        # Recursively find all the image files from the path label_path
-        for lbl_path in glob.glob(label_path+"/**/*"):
-            lbl_paths.append(lbl_path)
+    elif myaction == 2:
+        #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+        # Takes as input path to image file and returns 
+        # resized 3 channel RGB image of as numpy array of size (256, 256, 3)
+        def getPic(img_path):
+            return np.array(Image.open(img_path).convert('RGB').resize((256,256),Image.ANTIALIAS))
+
+        # Return the images and corresponding labels as numpy arrays
+        def get_ds(data_path, label_path):
+            img_paths = list()
+            lbl_paths = list()
             
-        images = np.zeros((len(img_paths),256,256,3))
-        labels = np.zeros((len(lbl_paths),256,256,3))
-          
-        # Read and resize the images
-        # Get the encoded labels
-        for i, img_path in enumerate(img_paths):
-            images[i] = getPic(img_path)
-                  
-        for i, lbl_path in enumerate(lbl_paths):
-            labels[i] = getPic(lbl_path)
+            # Recursively find all the image files from the path data_path
+            for img_path in glob.glob(data_path+"/**/*"):
+                img_paths.append(img_path)
             
-        return images,labels
+            # Recursively find all the image files from the path label_path
+            for lbl_path in glob.glob(label_path+"/**/*"):
+                lbl_paths.append(lbl_path)
+                
+            images = np.zeros((len(img_paths),256,256,3))
+            labels = np.zeros((len(lbl_paths),256,256,3))
+              
+            # Read and resize the images
+            # Get the encoded labels
+            for i, img_path in enumerate(img_paths):
+                images[i] = getPic(img_path)
+                      
+            for i, lbl_path in enumerate(lbl_paths):
+                labels[i] = getPic(lbl_path)
+                
+            return images,labels
 
-    train_X, train_y = get_ds("./data/endoscopic/train/image/", "./data/endoscopic/train/label/")
-    test_X, test_y = get_ds("./data/endoscopic/test/", "./data/endoscopic/test/")
+        train_X, train_y = get_ds("./data/endoscopic/train/image/", "./data/endoscopic/train/label/")
+        test_X, test_y = get_ds("./data/endoscopic/test/", "./data/endoscopic/test/")
 
-    model = unet()
-    model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-    model.fit(train_X,train_y,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
+        model = unet()
+        model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
+        model.fit(train_X,train_y,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
 
-    results = model.predict(testX)
+        results = model.predict(testX)
 
-    saveResult("data/endoscopic/test",results)
+        saveResult("data/endoscopic/test",results)
+
+    else:
+        print("Invalid action. Goodbye!")
+        
+else:
+    print("Invalid option. Goodbye!")
