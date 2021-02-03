ca4f78798a864c082ccf41ffb77083b8fcb562f4
DigeshC <chitrakar.digesh@gmail.com>
2021-02-03 12:53:20 -0500

    added results npy

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from data import *
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-n_test = 10
+n_test = 100
 
 data_gen_args = dict(rotation_range=0.2,
                     width_shift_range=0.05,
@@ -16,7 +16,7 @@ myGene = trainGenerator(2,'data/endoscopic/train','image','label',data_gen_args,
 
 model = unet()
 model_checkpoint = ModelCheckpoint('unet_endoscopic.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=30,epochs=1,callbacks=[model_checkpoint])
+model.fit_generator(myGene,steps_per_epoch=500,epochs=10,callbacks=[model_checkpoint])
 
 testGene = testGenerator("data/endoscopic/test", num_image = n_test, flag_multi_class = True, as_gray = False)
 results = model.predict_generator(testGene,n_test,verbose=1)
